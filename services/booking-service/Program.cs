f2b04d0416405fa6a8495b8c31e1075776ea7d40
Tushar Sharma <tusharforcourses@gmail.com>
2025-06-10 22:38:02 +0530

    generate ids automatically and increment by 1 everytime

--- services/booking-service/Program.cs
+++ services/booking-service/Program.cs
@@ -6,8 +6,13 @@ var app = builder.Build();
 // In-memory list to store bookings
 var bookings = new List<Booking>();
 
+int nextId = 1;
+
 app.MapPost("/bookings", (Booking booking) =>
 {
+    booking.Id = nextId++;
+    booking.BookingTime = DateTime.UtcNow;
+
     bookings.Add(booking);
     return Results.Created($"/bookings/{booking.Id}", booking);
 });
