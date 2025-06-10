164ca0f367b63242beb16ab2b65b9945d1ba71e2
Tushar Sharma <tusharforcourses@gmail.com>
2025-06-10 23:08:25 +0530

    integrate booking-service with flight-service

---
+++ services/booking-service/Models/SeatReservationRequest.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace booking_service.Models
+{
+    public class SeatReservationRequest
+    {
+        public int SeatsToReserve { get; set; }
+    }
+}
--- services/booking-service/Program.cs
+++ services/booking-service/Program.cs
@@ -1,15 +1,26 @@
 using booking_service.Models;
+using System.Net.Http.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // In-memory list to store bookings
 var bookings = new List<Booking>();
-
 int nextId = 1;
 
-app.MapPost("/bookings", (Booking booking) =>
+string flightServiceBaseUrl = "http://localhost:5290";
+
+app.MapPost("/bookings", async(Booking booking) =>
 {
+    var httpClient = new HttpClient();
+    var reserveUrl = $"{flightServiceBaseUrl}/flights/{booking.FlightId}/reserve";
+    var response = await httpClient.PutAsJsonAsync(reserveUrl , new {seatsToReserve = booking.SeatsReserved});
+
+    if(!response.IsSuccessStatusCode)
+    {
+        return Results.BadRequest($"Failed to reserve seats for flight {booking.FlightId}. Reason: {await response.Content.ReadAsStringAsync()}");
+    }
+
     booking.Id = nextId++;
     booking.BookingTime = DateTime.UtcNow;
 
