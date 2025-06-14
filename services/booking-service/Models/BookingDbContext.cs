3440fd2bad5353da6ac3cbe18511294e34856fa5
Tushar Sharma <tusharforcourses@gmail.com>
2025-06-14 18:20:12 +0530

    Add EF support and replace in-memory db with SQLite

---
+++ services/booking-service/Models/BookingDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace booking_service.Models
+{
+    public class BookingDbContext : DbContext
+    {
+        public BookingDbContext(DbContextOptions<BookingDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Booking> Bookings {get; set;}
+    }
+}
