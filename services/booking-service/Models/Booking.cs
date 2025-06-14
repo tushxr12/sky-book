34d5780a5e17c4d44164c7e27dd824c682471e7c
Tushar Sharma <tusharforcourses@gmail.com>
2025-06-14 18:22:14 +0530

    Add validations for booking service

--- services/booking-service/Models/Booking.cs
+++ services/booking-service/Models/Booking.cs
@@ -1,13 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace booking_service.Models
 {
     public class Booking
     {
         public int Id { get; set; }
+
+        [Required]
         public int FlightId { get; set; }
+
+        [Required]
+        [MinLength(2)]
         public string PassengerName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "At least 1 seat must be reserved")]
         public int SeatsReserved { get; set; }
+        
         public DateTime BookingTime { get; set; } = DateTime.UtcNow;
     }
 }
--- services/booking-service/Program.cs
+++ services/booking-service/Program.cs
@@ -1,17 +1,43 @@
 using booking_service.Models;
 using System.Net.Http.Json;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add EF Core with SQLite
+builder.Services.AddDbContext<BookingDbContext>(options =>
+    options.UseSqlite("Data Source=bookings.db"));
+
 var app = builder.Build();
 
+// Run EF migrations (create DB file if it doesn't exist)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BookingDbContext>();
+    db.Database.EnsureCreated();
+}
+
 // In-memory list to store bookings
-var bookings = new List<Booking>();
-int nextId = 1;
+// var bookings = new List<Booking>();
+
+
+// int nextId = 1;
 
 string flightServiceBaseUrl = "http://localhost:5290";
 
-app.MapPost("/bookings", async(Booking booking) =>
+app.MapPost("/bookings", async(Booking booking, BookingDbContext db) =>
 {
+    //Validating manually
+    var validationContext = new ValidationContext(booking);
+    var validationResults = new List<ValidationResult>();
+
+    if(!Validator.TryValidateObject(booking, validationContext, validationResults, true))
+    {
+        return Results.BadRequest(validationResults);
+    }
+
+    //Calling flight service below to reserve seats
     var httpClient = new HttpClient();
     var reserveUrl = $"{flightServiceBaseUrl}/flights/{booking.FlightId}/reserve";
     var response = await httpClient.PutAsJsonAsync(reserveUrl , new {seatsToReserve = booking.SeatsReserved});
@@ -21,16 +47,19 @@ app.MapPost("/bookings", async(Booking booking) =>
         return Results.BadRequest($"Failed to reserve seats for flight {booking.FlightId}. Reason: {await response.Content.ReadAsStringAsync()}");
     }
 
-    booking.Id = nextId++;
-    booking.BookingTime = DateTime.UtcNow;
+    // booking.Id = nextId++; We donot this with EF as it'll be taken care
 
-    bookings.Add(booking);
+    booking.BookingTime = DateTime.UtcNow;
+    db.Bookings.Add(booking);
+    
+    await db.SaveChangesAsync();
+
+    // bookings.Add(booking);
     return Results.Created($"/bookings/{booking.Id}", booking);
 });
 
-app.MapGet("/bookings" , ()=>
-{
-    return Results.Ok(bookings);
-});
+app.MapGet("/bookings" , async(BookingDbContext db)=>
+    await db.Bookings.ToListAsync());
+
 
 app.Run();
